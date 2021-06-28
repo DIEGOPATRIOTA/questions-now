31bd307876877a29d5ac00c709159c8932f30b4b
Diego Patriota <diego.patriota@saqueepague.com.br>
2021-06-28 20:03:42 -0300

    creating a room, enter the room, add authorization, creating and ask questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 
 import { Home } from "./pages/Home";
 import { NewRoow } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home}/>
-        <Route path="/rooms/new" component={NewRoow}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" exact component={NewRoow}/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
       </AuthContextProvider>      
     </BrowserRouter>
   );
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { FormEvent, useState } from "react";
 //import { useAuth } from "../hooks/useAuth";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import '../styles/auth.scss';
 import { Button } from "../compoments/Button";
+import { database } from "../services/firebase";
+import { useAuth } from "../hooks/useAuth";
 
 export function NewRoow() {
     //const { user, signInWithGoogle } = useAuth();
-    
+    const { user } = useAuth();
+    const history = useHistory();
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if ( newRoom.trim() === '' ) {
+            return;
+        } 
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authrId: user?.id
+        });
+
+        history.push(`/room/${firebaseRoom.key}`)
+    }
+
     return(
         <div id="page-auth">
             <aside>
@@ -20,10 +43,12 @@ export function NewRoow() {
                 <div>
                     <img src={logoImg} alt="questionsnow" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input 
                           type="text" 
                           placeholder="Nome da sala"
+                          onChange={event => setNewRoom(event.target.value)}
+                          value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
